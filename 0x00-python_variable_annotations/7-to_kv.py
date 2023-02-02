acfd3d5cd4aec5df4043366b3792fa55b93fd3bd
Teepee78 <eniolatoluwalase78@gmail.com>
2023-02-02 18:27:24 +0100

    Update

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,10 +2,10 @@
 """
 Defines the function to_kv
 """
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Returns a tuple
 
     Args:
