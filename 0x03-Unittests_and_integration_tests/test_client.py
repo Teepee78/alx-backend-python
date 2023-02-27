7034e28e9a1b4f964e706aee7600f07e0c7100fd
Teepee78 <eniolatoluwalase78@gmail.com>
2023-02-27 12:00:19 +0100

    Update

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+from typing import Dict
+from unittest import TestCase
+from unittest.mock import MagicMock, Mock, PropertyMock, patch
+
+from parameterized import parameterized, parameterized_class
+from requests import HTTPError
+
+from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+
+
+class TestGithubOrgClient(TestCase):
+    """Tests GithubOrgClient class"""
+
+    @parameterized.expand([
+        ("google", {"login": "google"}),
+        ("abc", {"login": "abc"})
+    ])
+    @patch("client.get_json")
+    def test_org(self, org: str, resp: Dict[str, str], mocked: MagicMock):
+        """Tests GithubOrgClient.org method"""
+
+        mocked.return_value = MagicMock(return_value=resp)
+        gh = GithubOrgClient(org)
+
+        self.assertEqual(gh.org(), resp)
+        mocked.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
+
+    def test_public_repos_url(self):
+        """Tests GithubOrgClient._public_repos_url"""
+
+        with patch(
+            "client.GithubOrgClient.org",
+            new_callable=PropertyMock
+        ) as org:
+            org.return_value = {
+                "repos_url": "https://api.github.com/users/google/repos"
+            }
+
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos"
+            )
+
+    @patch("client.get_json")
+    def test_public_repos(self, prop: PropertyMock):
+        """Tests GithubOrgClient.public_repos"""
+        payload = {
+            'repos_url': "https://api.github.com/users/google/repos",
+            'repos': [
+                {
+                    "id": 7697149,
+                    "name": "episodes.dart",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/episodes.dart",
+                    "created_at": "2013-01-19T00:31:37Z",
+                    "updated_at": "2019-09-23T11:53:58Z",
+                    "has_issues": True,
+                    "forks": 22,
+                    "default_branch": "master",
+                },
+                {
+                    "id": 8566972,
+                    "name": "kratu",
+                    "private": False,
+                    "owner": {
+                        "login": "google",
+                        "id": 1342004,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/google/kratu",
+                    "created_at": "2013-03-04T22:52:33Z",
+                    "updated_at": "2019-11-15T22:22:16Z",
+                    "has_issues": True,
+                    "forks": 32,
+                    "default_branch": "master",
+                },
+            ]
+        }
+        prop.return_value = payload["repos"]
+
+        with patch(
+            "client.GithubOrgClient._public_repos_url",
+            new_callable=PropertyMock
+        ) as url:
+            url.return_value = payload["repos_url"]
+            self.assertEqual(
+                GithubOrgClient("google").public_repos(),
+                [
+                    "episodes.dart",
+                    "kratu",
+                ],
+            )
+            url.assert_called_once()
+
+        prop.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(
+        self,
+        repo: Dict[str, Dict],
+        key: str,
+        output: bool
+    ):
+        """Tests GithubOrgClient.has_license method"""
+
+        self.assertEqual(GithubOrgClient.has_license(repo, key), output)
+
+
+@parameterized_class([
+    {
+        'org_payload': TEST_PAYLOAD[0][0],
+        'repos_payload': TEST_PAYLOAD[0][1],
+        'expected_repos': TEST_PAYLOAD[0][2],
+        'apache2_repos': TEST_PAYLOAD[0][3]
+    },
+])
+class TestIntegrationGithubOrgClient(TestCase):
+    """Integration tests for GithubOrgClient class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Sets up class before running tests"""
+
+        route_payload = {
+            'https://api.github.com/orgs/google': cls.org_payload,
+            'https://api.github.com/orgs/google/repos': cls.repos_payload,
+        }
+
+        def get_payload(url):
+            if url in route_payload:
+                return Mock(**{'json.return_value': route_payload[url]})
+            return HTTPError
+
+        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """Tests the public_repos method."""
+
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos,
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """Tests the `public_repos` method with a license."""
+
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """Removes the class fixtures after running all tests."""
+        cls.get_patcher.stop()
---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+"""Test cases for utils module"""
+from typing import Any, Mapping, Sequence
+from unittest import TestCase
+from unittest.mock import Mock, patch
+
+from parameterized import parameterized, parameterized_class
+
+from utils import access_nested_map, get_json, memoize
+
+
+class TestAccessNestedMap(TestCase):
+    """Tests utils.access_nested_map function"""
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(
+        self,
+        nested_map: Mapping,
+        path: Sequence,
+        output: Any
+    ):
+        """Tests access_nested_map function return value"""
+        self.assertEqual(access_nested_map(nested_map, path), output)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError)
+    ])
+    def test_access_nested_map_exception(
+        self,
+        nested_map: Mapping,
+        path: Sequence,
+        exception: Exception
+    ):
+        """Tests access_nested_map function raises exception"""
+        with self.assertRaises(exception):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """Tests utils.get_json function"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, input: str, output: Mapping):
+        ret = {"json.return_value": output}
+        with patch("requests.get", return_value=Mock(**ret)) as r:
+            self.assertEqual(get_json(input), output)
+            r.assert_called_once_with(input)
+
+
+class TestMemoize(TestCase):
+    """Tests utils.memoize function"""
+
+    def test_memoize(self):
+        """Tests memoized function is called only once"""
+        class TestClass:
+            """Test class"""
+
+            def a_method(self):
+                """A method"""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """A property"""
+                return self.a_method()
+
+        with patch.object(
+            TestClass,
+            "a_method",
+            return_value=lambda: 42
+        ) as r:
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
+            r.assert_called_once()
